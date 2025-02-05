47ecbf9e9bf3f7b87f2964b2170f7221a9589f6b
sild.johann@gmail.com <sild.johann@gmail.com>
2025-02-05 11:10:44 +0200

    muut

---
+++ kaardialgoritm.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+import time
+
+### Constants ###
+
+# Adaptive threshold levels
+BKG_THRESH = 60
+CARD_THRESH = 30
+
+# Width and height of card corner, where rank and suit are
+CORNER_WIDTH = 32
+CORNER_HEIGHT = 84
+
+# Dimensions of rank train images
+RANK_WIDTH = 70
+RANK_HEIGHT = 125
+
+# Dimensions of suit train images
+SUIT_WIDTH = 70
+SUIT_HEIGHT = 100
+
+RANK_DIFF_MAX = 2000
+SUIT_DIFF_MAX = 700
+
+CARD_MAX_AREA = 120000
+CARD_MIN_AREA = 25000
+
+font = cv2.FONT_HERSHEY_SIMPLEX
