fe496141c3e1a8c970cdfb24af7b70c964b40102
TauriProge <tauri.tagu@real.edu.ee>
2025-02-05 11:29:44 +0200

    q

--- mainc.py
+++ mainc.py
@@ -21,24 +21,24 @@ while True:
 
     t1 = cv2.getTickCount()
     pre_proc = Cards.preprocess_image(frame)
-    cnts_sort, cnt_is_card = Cards.find_cards(pre_proc)
-    if len(cnts_sort) != 0:
-        cards = []
-        k = 0
-        for i in range(len(cnts_sort)):
-            if (cnt_is_card[i] == 1):
-                cards.append(Cards.preprocess_card(cnts_sort[i],frame))
-                cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff = Cards.match_card(cards[k],train_ranks,train_suits)
-                frame = Cards.draw_results(frame, cards[k])
-                k = k + 1
-    if (len(cards) != 0):
-        temp_cnts = []
-        for i in range(len(cards)):
-            temp_cnts.append(cards[i].contour)
-        cv2.drawContours(frame,temp_cnts, -1, (255,0,0), 2)
-        cv2.putText(frame,"FPS: "+str(int(frame_rate_calc)),(10,26),font,0.7,(255,0,255),2,cv2.LINE_AA)
+    # cnts_sort, cnt_is_card = Cards.find_cards(pre_proc)
+    # if len(cnts_sort) != 0:
+    #     cards = []
+    #     k = 0
+    #     for i in range(len(cnts_sort)):
+    #         if (cnt_is_card[i] == 1):
+    #             cards.append(Cards.preprocess_card(cnts_sort[i],frame))
+    #             cards[k].best_rank_match,cards[k].best_suit_match,cards[k].rank_diff,cards[k].suit_diff = Cards.match_card(cards[k],train_ranks,train_suits)
+    #             frame = Cards.draw_results(frame, cards[k])
+    #             k = k + 1
+    # if (len(cards) != 0):
+    #     temp_cnts = []
+    #     for i in range(len(cards)):
+    #         temp_cnts.append(cards[i].contour)
+    #     cv2.drawContours(frame,temp_cnts, -1, (255,0,0), 2)
+    #     cv2.putText(frame,"FPS: "+str(int(frame_rate_calc)),(10,26),font,0.7,(255,0,255),2,cv2.LINE_AA)
 
-    cv2.imshow("Card Detector",frame)
+    cv2.imshow("Card Detector",pre_proc)
 
     t2 = cv2.getTickCount()
     time1 = (t2-t1)/freq
